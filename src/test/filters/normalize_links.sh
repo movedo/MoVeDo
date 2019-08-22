1986e33959746ac988f4dc3625f6cfb10af9ea2f
hoijui <hoijui.quaero@gmail.com>
2019-08-22 16:42:19 +0200

    add the normalize_links filter

---
+++ src/test/filters/normalize_links.sh
@@ -0,0 +1,125 @@
+#!/bin/sh
+
+this_scripts_dir_rel=`dirname $0`
+. "$this_scripts_dir_rel/_common.sh"
+
+cat > "$in_file" << EOF
+## URLs
+
+[link-url-abs-1](http://www.google.com/dir/file.md)
+[link-url-abs-2](ssh://git@github.com/user/repo/dir/file.md)
+[link-url-abs-3](ftp://www.google.com/dir/file.md)
+[link-url-abs-4](ftp://user@www.google.com/dir/file.md)
+[link-url-abs-5](strange-protocol://user@www.google.com/dir/file.md)
+[link-url-abs-6](wierd_protocol://user@www.google.com/dir/file.md)
+[link-url-abs-7](double:protocol://user@www.google.com/dir/file.md)
+[link-url-abs-8](double::protocol://user@www.google.com/dir/file.md)
+
+[link-url-abs-ref-1](http://www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-2](ssh://git@github.com/user/repo/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-3](ftp://www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-4](ftp://user@www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-5](strange-protocol://user@www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-6](wierd_protocol://user@www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-7](double:protocol://user@www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-8](double::protocol://user@www.google.com/dir/file.md#in-page-ref-99)
+
+## Local
+
+### Absolute
+
+[link-url-abs-loc-1](/root/dir/file.md)
+[link-url-abs-loc-2](C:/root/dir/file.md)
+[link-url-abs-loc-3](D:/root/dir/file.md)
+[link-url-abs-loc-4](/root/dir/../file.md)
+[link-url-abs-loc-5](C:/root/../dir/file.md)
+[link-url-abs-loc-6](D:/root/dir/../file.md)
+[link-url-abs-loc-7](/root/dir/../subdir/.././file.md)
+
+[link-url-abs-loc-ref-1](/root/dir/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-2](C:/root/dir/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-3](D:/root/dir/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-4](/root/dir/../file.md#in-page-ref-99)
+[link-url-abs-loc-ref-5](C:/root/../dir/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-6](D:/root/dir/../file.md#in-page-ref-99)
+[link-url-abs-loc-ref-7](/root/dir/../subdir/.././file.md#in-page-ref-99)
+
+### Relative
+
+[link-url-rel-1](some/prefix/./relative/dir/file.md)
+[link-url-rel-2](some/prefix/relative/dir/file.md)
+[link-url-rel-3](some/prefix/../relative/dir/file.md)
+[link-url-rel-5](some/prefix/../../relative/dir/file.md)
+[link-url-rel-6](some/prefix/./../../relative/./dir/file.md)
+[link-url-rel-7](some/prefix/../../../relative/./dir/file.md)
+
+[link-url-rel-ref-1](some/prefix/./relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-2](some/prefix/relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-3](some/prefix/../relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-5](some/prefix/../../relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-6](some/prefix/./../../relative/./dir/file.md#in-page-ref-99)
+[link-url-rel-ref-7](some/prefix/../../../relative/./dir/file.md#in-page-ref-99)
+EOF
+
+cat > "$exp_out_file" << EOF
+## URLs
+
+[link-url-abs-1](http://www.google.com/dir/file.md)
+[link-url-abs-2](ssh://git@github.com/user/repo/dir/file.md)
+[link-url-abs-3](ftp://www.google.com/dir/file.md)
+[link-url-abs-4](ftp://user@www.google.com/dir/file.md)
+[link-url-abs-5](strange-protocol://user@www.google.com/dir/file.md)
+[link-url-abs-6](wierd_protocol://user@www.google.com/dir/file.md)
+[link-url-abs-7](double:protocol://user@www.google.com/dir/file.md)
+[link-url-abs-8](double::protocol://user@www.google.com/dir/file.md)
+
+[link-url-abs-ref-1](http://www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-2](ssh://git@github.com/user/repo/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-3](ftp://www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-4](ftp://user@www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-5](strange-protocol://user@www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-6](wierd_protocol://user@www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-7](double:protocol://user@www.google.com/dir/file.md#in-page-ref-99)
+[link-url-abs-ref-8](double::protocol://user@www.google.com/dir/file.md#in-page-ref-99)
+
+## Local
+
+### Absolute
+
+[link-url-abs-loc-1](/root/dir/file.md)
+[link-url-abs-loc-2](C:/root/dir/file.md)
+[link-url-abs-loc-3](D:/root/dir/file.md)
+[link-url-abs-loc-4](/root/file.md)
+[link-url-abs-loc-5](C:/dir/file.md)
+[link-url-abs-loc-6](D:/root/file.md)
+[link-url-abs-loc-7](/root/file.md)
+
+[link-url-abs-loc-ref-1](/root/dir/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-2](C:/root/dir/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-3](D:/root/dir/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-4](/root/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-5](C:/dir/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-6](D:/root/file.md#in-page-ref-99)
+[link-url-abs-loc-ref-7](/root/file.md#in-page-ref-99)
+
+### Relative
+
+[link-url-rel-1](some/prefix/relative/dir/file.md)
+[link-url-rel-2](some/prefix/relative/dir/file.md)
+[link-url-rel-3](some/relative/dir/file.md)
+[link-url-rel-5](relative/dir/file.md)
+[link-url-rel-6](relative/dir/file.md)
+[link-url-rel-7](../relative/dir/file.md)
+
+[link-url-rel-ref-1](some/prefix/relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-2](some/prefix/relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-3](some/relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-5](relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-6](relative/dir/file.md#in-page-ref-99)
+[link-url-rel-ref-7](../relative/dir/file.md#in-page-ref-99)
+EOF
+
+run_filter
+
+eval_result
+
