67e383bd511ae7db55a51ca3aed917427acdbad9
hoijui <hoijui.quaero@gmail.com>
2020-09-21 20:03:44 +0200

    scripts: lots of minor code style cleanup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -36,46 +36,46 @@ _set_if_unset() {
 
 script_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 # Root of the local MoVeDo (doc build tool) root directory
-_set_if_unset movedo_root_dir "`cd "$script_dir/.."; pwd`"
+movedo_root_dir="${movedo_root_dir:-"$(cd "$script_dir/.."; pwd)"}"
 # Where to look for the Python panflute Pandoc filters
 export filters_dir="$movedo_root_dir/filters"
 # The Projects root dir
-_set_if_unset proj_dir `pwd`
+proj_dir="${proj_dir:-"$(pwd)"}"
 # Root directory for all files created during the documentation build process
-_set_if_unset build_dir_rel "build"
-_set_if_unset build_dir "$proj_dir/$build_dir_rel"
+build_dir_rel="${build_dir_rel:-"build"}"
+build_dir="${build_dir:-"$proj_dir/$build_dir_rel"}"
 # YAML meta-data, to be usedin the single, fused Markdown file as FrontMatter
-_set_if_unset doc_meta_file "$proj_dir/doc.yml"
+doc_meta_file="${doc_meta_file:-"$proj_dir/doc.yml"}"
 # BibTex meta-data, to be usedin the PDF output (optional)
-_set_if_unset biblography_file "$proj_dir/citations.bib"
-_set_if_unset templates_dir "$movedo_root_dir/templates"
+biblography_file="${biblography_file:-"$proj_dir/citations.bib"}"
+templates_dir="${templates_dir:-"$movedo_root_dir/templates"}"
 # Root for all generated sources
 # the original sources are copied in here too,
 # and the documentation is actually built from here
-_set_if_unset gen_src_dir "$build_dir/gen_sources"
+gen_src_dir="${gen_src_dir:-"$build_dir/gen_sources"}"
 # Index file(s) containing the Markdonw sources
 # to be used for linearizing, in the correct order.
-_set_if_unset index_md_file_name "index-md.txt"
-_set_if_unset index_md_file_generated "$gen_src_dir/$index_md_file_name"
-_set_if_unset index_md_file_manual "$proj_dir/$index_md_file_name"
+index_md_file_name="${index_md_file_name:-"index-md.txt"}"
+index_md_file_generated="${index_md_file_generated:-"$gen_src_dir/$index_md_file_name"}"
+index_md_file_manual="${index_md_file_manual:-"$proj_dir/$index_md_file_name"}"
 # If the manually crafted file one is present,
 # it has precedence over the generated one.
-_set_if_unset index_md_file "`[ -f "$index_md_file_manual" ] && echo "$index_md_file_manual" || echo "$index_md_file_generated"`"
-_set_if_unset html_dir "$build_dir/html"
-_set_if_unset pdf_dir "$build_dir/pdf"
-_set_if_unset single_md "$build_dir/doc.md"
-_set_if_unset single_pdf "$pdf_dir/doc.pdf"
+index_md_file="${index_md_file:-"$([ -f "$index_md_file_manual" ] && echo "$index_md_file_manual" || echo "$index_md_file_generated")"}"
+html_dir="${html_dir:-"$build_dir/html"}"
+pdf_dir="${pdf_dir:-"$build_dir/pdf"}"
+single_md="${single_md:-"$build_dir/doc.md"}"
+single_pdf="${single_pdf:-"$pdf_dir/doc.pdf"}"
 # Open results (like PDF files or index.html) with the systems default viewer/editor
 # default: "no"
 # set to any non empty value for "yes"
-_set_if_unset OPEN_X11 ""
+OPEN_X11="${OPEN_X11:-""}"
 
 _check_tool() {
-	tool="$@"
+	local tool="$*"
 
 	if ! which "$tool" > /dev/null 2>&1
 	then
-		_error "'$tool' is not installed. Please install it manually, or run '`dirname $0`/setup'"
+		_error "'$tool' is not installed. Please install it manually, or run '$(dirname "$0")/setup'"
 	fi
 }
 
@@ -92,7 +92,7 @@ _check_env() {
 
 	if [ ! -e "$gen_src_dir" ]
 	then
-		_error "Generated sources dir not found, please run '`dirname $0`/generate'"
+		_error "Generated sources dir not found, please run '$(dirname "$0")/generate'"
 	fi
 }
 
@@ -110,11 +110,13 @@ _contains_word() {
 }
 
 _permanently_add_to_path() {
-	add_path="$1"
+	local add_path="$1"
 	# HACK We probably should not modify the users environment permanently and globally like that
-	echo '' >> "$HOME/.profile"
-	echo 'export PATH="$PATH:'"$add_path"'"' >> "$HOME/.profile"
-	echo '' >> "$HOME/.profile"
+	{
+		echo ''
+		echo "export PATH=\"\$PATH:$add_path\""
+		echo ''
+	} >> "$HOME/.profile"
 	export PATH="$PATH:$add_path"
 }
 
@@ -122,7 +124,7 @@ _permanently_add_to_path() {
 # return the git commit description.
 # Else, return "<UNKNOWN>".
 _fetch_version() {
-	doc_version="<UNKNOWN>"
+	local doc_version="<UNKNOWN>"
 	if which git &> /dev/null && git rev-parse --is-inside-work-tree &> /dev/null
 	then
 		doc_version="$(git describe --always --dirty --broken)"
@@ -135,8 +137,9 @@ _fetch_version() {
 # meaning that no uncommitted or changed files are around.
 # Else, return false.
 _is_repo_clean() {
+	local doc_version
 	doc_version="$(_fetch_version)"
-	repo_clean=0
+	local repo_clean=0
 	if [ "$doc_version" = "<UNKNOWN>" ] || [[ "$doc_version" = *-dirty ]] || [[ "$doc_version" = *-broken ]]
 	then
 		repo_clean=1
